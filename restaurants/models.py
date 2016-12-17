6bfb1c76cc0f93f833c5c09ee19532d49ea3a6a8
Dan Reich <dancreich@gmail.com>
2016-12-17 18:12:12 -0500

    Move join class visit to restaurant app to avoid circular dependency."

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,8 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Restaurant(models.Model):
     name = models.CharField(max_length=200)
+    visitors = models.ManyToManyField(User, through='Visit')
 
     def __str__(self):
         return self.name
+
+class Visit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
--- users/models.py
+++ users/models.py
@@ -1,8 +1,4 @@
 from django.db import models
 from django.contrib.auth.models import User
-from restaurants.models import Restaurant
 
 # Create your models here.
-class RestaurantVisit(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
