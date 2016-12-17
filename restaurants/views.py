cc69cbcb931fdc74a31676f446e8f882c6521900
Dan Reich <dancreich@gmail.com>
2016-12-17 13:23:13 -0500

    Add basic restaurant model and index page.

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from restaurants.models import Restaurant
 
 # Create your views here.
+def index(request):
+    restaurants = Restaurant.objects.all()
+    return HttpResponse(restaurants)
