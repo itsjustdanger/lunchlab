80136cb9a74a6e83225243caaeea723ce389f022
Dan Reich <dancreich@gmail.com>
2016-12-20 10:53:11 -0500

    Modify gitignore.

--- lunchlab/views.py
+++ lunchlab/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.urls import reverse
 from restaurants.models import Restaurant
 from reviews.models import Review
 
