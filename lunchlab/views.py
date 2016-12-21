34fe7e9210804b23d621adf56ffd9437baafaa7d
Dan Reich <dancreich@gmail.com>
2016-12-20 22:00:25 -0500

    Add detailed header styling.

--- lunchlab/views.py
+++ lunchlab/views.py
@@ -9,11 +9,13 @@ def restaurant_detail(request, id):
     try:
         restaurant = user.visited_restaurants.get(id=id)
         restaurant.visited = True
+        restaurant.user_reviewed = not not Review.objects.filter(user_id=user.id, restaurant_id=id)
     except:
         restaurant = get_object_or_404(Restaurant, pk=id)
         restaurant.visited = False
+        restaurant.user_reviewed = False
 
     reviews = Review.objects.filter(restaurant_id=id)
-    restaurant.user_reviewed = not not Review.objects.filter(user_id=user.id, restaurant_id=id)
+
 
     return render(request, 'restaurants/show.html', {'restaurant': restaurant, 'reviews': reviews})
