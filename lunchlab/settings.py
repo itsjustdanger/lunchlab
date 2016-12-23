e001ff5fe384b407b9ff835af4f43af03468cb80
Dan Reich <dancreich@gmail.com>
2016-12-23 17:52:56 -0500

    Add additional hosts.

--- lunchlab/settings.py
+++ lunchlab/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'aanjlc!p21c9cm%uzmmp69k#d)-e6=xv)0%pbc-js0gam+=mt4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['young-oasis-28629.herokuapp.com']
 
 
 # Application definition
