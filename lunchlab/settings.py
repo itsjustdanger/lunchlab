7d81eff9705df9b47200f19713f40631b33c3b91
Dan Reich <dancreich@gmail.com>
2016-12-23 18:40:18 -0500

    Modify settings file.

--- lunchlab/settings.py
+++ lunchlab/settings.py
@@ -142,7 +142,6 @@ USE_TZ = True
 AWS_ACCESS_KEY_ID = os.environ['S3_KEY']
 AWS_SECRET_ACCESS_KEY = os.environ['S3_SECRET']
 
-
 # Tell django-storages that when coming up with the URL for an item in S3 storage, keep
 # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
 # This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
